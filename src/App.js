9198d5928053c2d100d119e1aaf6573aabe824a5
Jose Miguel Navarro <jmn8718@gmail.com>
2017-03-24 17:29:37 +0100

    added mdc lib;

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import '@material/toolbar/dist/mdc.toolbar.css';
+import '@material/layout-grid/dist/mdc.layout-grid.css';
 import GoogleMapReact from 'google-map-react';
 
 const MapBootstrapURLKeys = {
@@ -39,18 +40,30 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <GoogleMapReact
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-          options={MapOptions}
-          bootstrapURLKeys={MapBootstrapURLKeys}
-          onChange={onBoundsChange}
-          onClick={onClick}
-          resetBoundsOnResize
-        >
-        <Marker {...this.props.center} text="RTVE"/>
-      </GoogleMapReact>
+      <div style={{ height: '100vh' }}>
+        <header className="mdc-toolbar mdc-toolbar--fixed">
+          <section className="mdc-toolbar__section mdc-toolbar__section--align-start">
+            <span className="mdc-toolbar__title">Title</span>
+          </section>
+        </header>
+        <div className="mdc-layout-grid" style={{ height: 'calc(100% - 64px)', top: 64, position: 'relative' }}>
+          <div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-4 mdc-layout-grid__cell--span-12-tablet">
+
+          </div>
+          <div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-8 mdc-layout-grid__cell--span-12-tablet">
+            <GoogleMapReact
+              defaultCenter={this.props.center}
+              defaultZoom={this.props.zoom}
+              options={MapOptions}
+              bootstrapURLKeys={MapBootstrapURLKeys}
+              onChange={onBoundsChange}
+              onClick={onClick}
+              resetBoundsOnResize
+              >
+              <Marker {...this.props.center} text="RTVE"/>
+            </GoogleMapReact>
+          </div>
+        </div>
       </div>
     );
   }
